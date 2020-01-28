87e164817ef0180df04ecad3402a965e5eafdad7
arianna choza <unachoza@gmail.com>
2020-01-28 16:04:33 -0500

    returned sounds fixing audio function

--- myscript.js
+++ myscript.js
@@ -2,51 +2,7 @@
 window.addEventListener('keydown', (e) => {
   const audio = document.querySelector(`'audio[data-key=${e.keyCode}]"`)
   console.log(audio)
+  if(!audio) return 
 
 })
-  // const clap = document.getElementById().
-  // .addEventListener('keydown', () => {
-  //   clap.body.style= "playing"
-  // })
   
-  //   hconst ihat = document.getElementById().
-  //   .addEventListener('keydown', () => {
-  //     clap.body.style= "playing"
-  //   })
-  
-  //   const kick = document.getElementById().
-  //   .addEventListener('keydown', () => {
-  //     clap.body.style= "playing"
-  //   })
-  
-  //   opeconst nhat = document.getElementById().
-  //   .addEventListener('keydown', () => {
-  //     clap.body.style= "playing"
-  //   })
-  
-  //   const boom = document.getElementById().
-  //   .addEventListener('keydown', () => {
-  //     clap.body.style= "playing"
-  //   })
-  
-  //   const ride = document.getElementById().
-  //   .addEventListener('keydown', () => {
-  //     clap.body.style= "playing"
-  //   })
-  
-  //   sconst nare = document.getElementById().
-  //   .addEventListener('keydown', () => {
-  //     clap.body.style= "playing"
-  //   })
-  
-  //  const  tom = document.getElementById().
-  //  .addEventListener('keydown', () => {
-  //    clap.body.style= "playing"
-  //  })
-  //  </div>
-  //  <div class="key">
-  //    <kbd> L</kbd>
-  //   const tink = document.getElementById().
-  //   .addEventListener('keydown', () => {
-  //     clap.body.style= "playing"
-  //   })
